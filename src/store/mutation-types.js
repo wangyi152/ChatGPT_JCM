8f26e91f67ee72dd3abba857b9a47cfb0d8256e6
wangyi152 <59157417+wangyi152@users.noreply.github.com>
2023-03-21 18:47:12 +0800

    Update mutation-types.js

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,6 +1,6 @@
 //AI头像地址设置
 export const AI_HEAD_IMG_URL="https://th.bing.com/th?id=ODL.3e2fbff4543f0d3632d34be6d02adc93&w=100&h=100&c=12&pcl=faf9f7&o=6&dpr=1.5&pid=13.1"
 //用户头像地址设置
-export const USER_HEAD_IMG_URL="https://account.microsoft.com/15968aa4-9f90-4d36-8bd9-707637a0204c"
+export const USER_HEAD_IMG_URL="https://q1.qlogo.cn/g?b=qq&nk=3291977101&s=640"
 //用户名称设置
 export const USER_NAME="sliverain"
